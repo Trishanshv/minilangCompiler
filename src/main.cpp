76607afb39219af8d0ea4e0bca1c264f51a3109f
trishansh <trishansh23@gmail.com>
2025-06-27 13:16:17 +0530

    progress has been made

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include "ast.hpp"
+#include<cstdio>
 
 extern int yyparse();
 extern std::unique_ptr<Program> root;
 extern FILE* yyin;
 
 int main(int argc, char** argv) {
-    if (argc > 1) {
-        yyin = fopen(argv[1], "r");
+    if (argc < 2) {
+        fprintf(stderr, "Using: %s <source file>\n", argv[0]);
+        return 1;
     }
-    if (yyparse() == 0 && root) {
-        root->print();
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        perror("fopen");
+        return 1;
     }
+
+    if (yyparse() == 0) {
+        if (root) {
+            root->print();
+        } else {
+            fprintf(stderr, "AST not generated.\n");
+        }
+    } else {
+        fprintf(stderr, "Parsing failed.\n");
+    }
+
+    fclose(yyin);
     return 0;
 }
